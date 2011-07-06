845673749de0f62c0012152b48fc795f681e9b4e
Kristin Stephens <ksteph@cs.berkeley.edu>
2011-07-06 12:27:05 -0700

    Element that calculates the latency deltas between the lines.

---
+++ calclatencydelta.cc
@@ -0,0 +1,101 @@
+// -*- c-basic-offset: 4 -*-
+#include <click/config.h>
+#include "calclatencydelta.hh"
+#include <click/error.hh>
+#include <click/confparse.hh>
+#include <click/packet_anno.hh>
+CLICK_DECLS
+
+CalcLatencyDelta::CalcLatencyDelta()
+    : _deltas(0), _last_latency(0), _max_paint(0), _offset(0)
+{
+}
+
+CalcLatencyDelta::~CalcLatencyDelta()
+{
+}
+
+int
+CalcLatencyDelta::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    if (cp_va_kparse(conf, this, errh,
+		     "OFFSET", cpkM, cpUnsigned, &_offset,
+		     "MAX_PAINT", cpkM, cpUnsigned, &_max_paint,
+		     cpEnd) < 0)
+	return -1;
+
+    _last_latency = new uint64_t[_max_paint];
+
+    _deltas = new int *[_max_paint];
+    for (unsigned int i = 0; i < _max_paint; i++) {
+        _deltas[i] = new int[_max_paint];
+
+        _last_latency[i] = 0;
+    }
+
+    return 0;
+}
+
+int
+CalcLatencyDelta::initialize(ErrorHandler *)
+{
+    return 0;
+}
+
+void
+CalcLatencyDelta::add_handlers()
+{
+}
+
+void
+CalcLatencyDelta::cleanup(CleanupStage)
+{
+    for (unsigned int i = 0; i < _max_paint; i++)
+        delete _deltas[i];
+    delete _deltas;
+    
+    delete _last_latency;
+}
+
+Packet *
+CalcLatencyDelta::simple_action(Packet *p)
+{
+    // _deltas[i][j] = latency_i - latency_j
+    int paint = PAINT_ANNO(p);
+    uint64_t src_timestamp = 0;
+    for (int i = 0; i < 8; i++)
+        src_timestamp |= (((p->data())[_offset+i]) << ((8-i-1)*8));
+    
+    uint64_t dst_timestamp = p->timestamp_anno().sec() * ((uint64_t) 1000000);
+    dst_timestamp += p->timestamp_anno().usec();
+
+    uint64_t latency = dst_timestamp - src_timestamp;
+    _last_latency[paint] = latency;
+
+    for (unsigned int i = 0; i < _max_paint; i++) {
+        if (_last_latency[i] != 0) {
+            _deltas[paint][i] = latency - _last_latency[i];
+            _deltas[i][paint] = _last_latency[i] - latency;
+        }
+    }
+
+    return p;
+}
+
+// Make sure to delete/free the data returned
+int**
+CalcLatencyDelta::get_deltas()
+{
+    int **deltas = new int *[_max_paint];
+    for (unsigned int i = 0; i < _max_paint; i++) {
+        deltas[i] = new int[_max_paint];
+
+        for (unsigned int j = 0; j < _max_paint; j++)
+            deltas[i][j] = _deltas[i][j];
+    }
+
+    return deltas;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(CalcLatencyDelta)
---
+++ calclatencydelta.hh
@@ -0,0 +1,65 @@
+// -*- c-basic-offset: 4 -*-
+#ifndef CLICK_CALCLATENCYDELTA_HH
+#define CLICK_CALCLATENCYDELTA_HH
+#include <click/element.hh>
+#include <click/glue.hh>
+CLICK_DECLS
+
+#define CLICK_CALCLATENCYDELTA_DEBUG
+#define LATENCY_THRESH 5000000 //Max latency of a line. 5s is enough I hope.
+
+/*
+  =c
+
+  CalcLatencyDelta(OFFSET, PAINT_MAX)
+
+  =s local
+
+  Calculates latency deltas between different packet paints.
+
+  =d
+
+  Using the timestamp found in the packet at OFFSET as the timestamp source and
+  the timestamp annotation as the dest timestamp it calculates the deltas between
+  the different packet paints. The timestamp in the packet (src timestamp) is
+  assumed to be a 64 bit timestamp in microseconds.
+
+  Keyword arguments:
+
+  =item OFFSET
+
+  The offset into the packet where the 64 bit timestamp in microseconds is.
+
+  =item PAINT_MAX
+
+  The max number of different paint colors there will be.
+ 
+*/
+
+class CalcLatencyDelta : public Element {
+public:
+    CalcLatencyDelta();
+    ~CalcLatencyDelta();
+
+    const char *class_name() const { return "CalcLatencyDelta"; }
+    const char *port_count() const { return PORTS_1_1; }
+    const char *processing() const { return AGNOSTIC; }
+  
+    int configure(Vector<String> &, ErrorHandler *);
+    int initialize(ErrorHandler *);
+    void add_handlers();
+    void cleanup(CleanupStage);
+
+    Packet *simple_action(Packet*);
+    int** get_deltas(); // Make sure to delete/free the data returned
+
+private:
+    int **_deltas;
+    uint64_t *_last_latency;
+
+    unsigned int _max_paint;
+    unsigned int _offset;
+};
+
+CLICK_ENDDECLS
+#endif
