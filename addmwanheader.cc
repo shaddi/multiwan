7ad183b9c355e9e13b219a022c082f1600444095
Kristin Stephens <ksteph@cs.berkeley.edu>
2011-07-06 14:13:48 -0700

    Changed the header adder element so it also included the bandwidth.

--- addmwanheader.cc
+++ addmwanheader.cc
@@ -1,6 +1,8 @@
 // -*- c-basic-offset: 4 -*-
 #include <click/config.h>
 #include "addmwanheader.hh"
+#include <click/confparse.hh>
+CLICK_DECLS
 
 AddMWanHeader::AddMWanHeader()
 {
@@ -11,15 +13,21 @@ AddMWanHeader::~AddMWanHeader()
 }
 
 int
-AddMWanHeader::configure(Vector<String> &, ErrorHandler *)
+AddMWanHeader::configure(Vector<String> &conf, ErrorHandler *errh)
 {
-  return 0;
+    if (cp_va_kparse(conf, this, errh,
+		     "DEFAULT_BW", cpkM, cpUnsigned, &_default_BW,
+		     cpEnd) < 0)
+	return -1;
+
+    return 0;
 }
 
 int
 AddMWanHeader::initialize(ErrorHandler *)
 {
-  return 0;
+    _default_BW = 0;
+    return 0;
 }
 
 void
@@ -35,18 +43,29 @@ AddMWanHeader::cleanup(CleanupStage)
 Packet *
 AddMWanHeader::simple_action(Packet *p)
 {
-  WritablePacket *q = p->push(8);
-  if (!q)
-    return 0;
+    // Timestamp + BW
+    WritablePacket *q = p->push(8+4);
+    if (!q)
+        return 0;
 
-  Timestamp now = Timestamp::now();
-  uint64_t now_us = now.sec() * ((uint64_t)(1000000));
-  now_us += now.usec();
+    // Timestamp
+    Timestamp now = Timestamp::now();
+    uint64_t now_us = now.sec() * ((uint64_t)(1000000));
+    now_us += now.usec();
 
-  uint64_t *tmp = (uint64_t*) q->data();
-  *tmp = now_us;
+    uint64_t *tmp = (uint64_t*) q->data();
+    *tmp = now_us;
 
-  return q;
+    // Bandwidth
+    uint32_t *p_bw = (uint32_t*) (q->data() + 8);
+    *p_bw = get_bandwidth();
+    return q;
+}
+
+uint32_t
+AddMWanHeader::get_bandwidth()
+{
+    return _default_BW;
 }
 
 CLICK_ENDDECLS
--- addmwanheader.hh
+++ addmwanheader.hh
@@ -8,43 +8,54 @@ CLICK_DECLS
 #define CLICK_ADDMWANHEADER_DEBUG
 
 /*
-=c
+  =c
 
-AddMWanHeader()
+  AddMWanHeader(DEFAULT_BW)
 
-=s local
+  =s local
 
-Adds a multiwan header to the front of the packet
+  Adds a multiwan header to the front of the packet
 
-=d
+  =d
 
-Adds a multiwan header to the front of the packet. The header is as follows:
+  Adds a multiwan header to the front of the packet. The header is as follows:
 
-0                   1                   2                   3
-0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                 Timestamp in microseconds                   |
-|                                                             |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
- */
+  0                   1                   2                   3
+  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                 Timestamp in microseconds                   |
+  |                                                             |
+  |-------------------------------------------------------------|
+  |          Bandwidth of this line in Bytes per second         |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+  Keyword arguemtsn:
+
+  =item DEFAULT_BW
+
+  The default bandwidth value in B/s.
+*/
 
 class AddMWanHeader : public Element {
 public:
-  AddMWanHeader();
-  ~AddMWanHeader();
+    AddMWanHeader();
+    ~AddMWanHeader();
 
-  const char *class_name() const { return "AddMWanHeader"; }
-  const char *port_count() const { return PORTS_1_1; }
-  const char *processing() const { return AGNOSTIC; }
+    const char *class_name() const { return "AddMWanHeader"; }
+    const char *port_count() const { return PORTS_1_1; }
+    const char *processing() const { return AGNOSTIC; }
   
-  int configure(Vector<String> &, ErrorHandler *);
-  int initialize(ErrorHandler *);
-  void add_handlers();
-  void cleanup(CleanupStage);
+    int configure(Vector<String> &, ErrorHandler *);
+    int initialize(ErrorHandler *);
+    void add_handlers();
+    void cleanup(CleanupStage);
 
-  Packet *simple_action(Packet*);
+    Packet *simple_action(Packet*);
 
 private:
+    uint32_t _default_BW;
+
+    uint32_t get_bandwidth();
 };
 
 CLICK_ENDDECLS
