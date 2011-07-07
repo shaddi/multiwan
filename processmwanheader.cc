4847989232c575227fffac68e983cf972ca7104a
Kristin Stephens <ksteph@cs.berkeley.edu>
2011-07-07 11:47:07 -0700

    Element that processes the header to find the bandwidth and then figures out the correct schedule for ProgScheduler element.

---
+++ processmwanheader.cc
@@ -0,0 +1,129 @@
+// -*- c-basic-offset: 4 -*-
+#include <click/config.h>
+#include "processmwanheader.hh"
+#include <click/error.hh>
+#include <click/confparse.hh>
+#include <click/packet_anno.hh>
+CLICK_DECLS
+
+ProcessMWanHeader::ProcessMWanHeader()
+    : _elem_ps(0), _max_paint(0), _bandwidths(0), _schedule(0)
+{
+}
+
+ProcessMWanHeader::~ProcessMWanHeader()
+{
+}
+
+int
+ProcessMWanHeader::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    Element *ps_element = 0;
+    if (cp_va_kparse(conf, this, errh,
+		     "PROGSCHEDULER", cpkM, cpElement, &ps_element,
+		     "MAX_PAINT", cpkM, cpUnsigned, &_max_paint,
+		     cpEnd) < 0)
+	return -1;
+
+    ProgScheduler *ps = 0;
+    if (ps_element && 
+        !(ps = (ProgScheduler *)(ps_element->cast("ProgScheduler"))))
+	return errh->error("PROGSCHEDULER must be a ProgScheduler element");
+    else if (ps)
+	_elem_ps = ps;
+
+    _bandwidths = new uint32_t[_max_paint];
+    _schedule = new int[_max_paint];
+
+    for (unsigned int i = 0; i < _max_paint; i++) {
+        _bandwidths[i] = 0;
+        _schedule[i] = 1;
+    }
+
+    return 0;
+}
+
+int
+ProcessMWanHeader::initialize(ErrorHandler *)
+{
+    return 0;
+}
+
+void
+ProcessMWanHeader::add_handlers()
+{
+}
+
+void
+ProcessMWanHeader::cleanup(CleanupStage)
+{
+    delete _bandwidths;
+    delete _schedule;
+}
+
+Packet *
+ProcessMWanHeader::simple_action(Packet *p)
+{
+    int paint = PAINT_ANNO(p);
+    uint32_t bw = 0;
+    for (int i = 0; i < 4; i++)
+        bw |= (((p->data())[8+i]) << ((8-i-1)*8));
+
+    _bandwidths[paint] = bw;
+
+    if (update_schedule(_max_paint, _bandwidths))
+        _elem_ps->set_schedule(_schedule);
+
+    return p;
+}
+
+/*
+  Returns true if schedule changes.
+
+  Takes the largest bandwidth value and makes that schedule value
+  MAX_SCH_VALUE. Then divides the largest value with with the max to get the
+  divider to find the proper proportions for all the others.
+ */
+bool
+ProcessMWanHeader::update_schedule(int size, uint32_t *bandwidths)
+{
+    int tmp_sch[size];
+
+    int max = 0;
+    int max_paint = 0;
+
+    for (int i = 0; i < size; i++)
+        if (max < bandwidths[i]) {
+            max = bandwidths[i];
+            max_paint = i;
+        }
+
+    int divisor = max/MAX_SCH_VALUE;
+
+    for (int i = 0; i < size; i++) {
+        int value = bandwidths[i]/divisor;
+        tmp_sch[i] = value > 0 ? value : MIN_SCH_VALUE;
+    }
+
+    if (!static_ary_equals(size, tmp_sch, _schedule)) {
+        for (int i = 0; i < size; i++)
+            _schedule[i] = tmp_sch[i];
+
+        return true;
+    }
+
+    return false;
+}
+
+bool
+ProcessMWanHeader::static_ary_equals(int size, int *a, int *b)
+{
+    for (int i = 0; i < size; i++)
+        if (a[i] != b[i])
+            return false;
+
+    return true;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(ProcessMWanHeader)
---
+++ processmwanheader.hh
@@ -0,0 +1,73 @@
+// -*- c-basic-offset: 4 -*-
+#ifndef CLICK_PROCESSMWANHEADER_HH
+#define CLICK_PROCESSMWANHEADER_HH
+#include <click/element.hh>
+#include <click/glue.hh>
+#include "progscheduler.hh"
+CLICK_DECLS
+
+#define CLICK_PROCESSMWANHEADER_DEBUG
+#define MAX_SCH_VALUE 10
+#define MIN_SCH_VALUE 1
+
+/*
+  =c
+
+  ProcessMWanHeader(PROGSCHEDULER, PAINT_MAX)
+
+  =s local
+
+  Processes the MWan header.
+
+  =d
+
+  Processes the MWan header. Especially looks at the bandwidth value in the
+  header so it can properly change the value of ProgScheduler.
+
+  The packets must have the paint annotation set so that it can tell which line
+  a packet is from. The paint value should match the output port for the
+  ProgScheduler.
+
+  Keyword arguments:
+
+  =item PROGSCHEDULER
+
+  The ProgScheduler this element should control.
+
+  =item PAINT_MAX
+
+  The max number of different paint colors there will be.
+ 
+ */
+
+class ProcessMWanHeader : public Element {
+public:
+    ProcessMWanHeader();
+    ~ProcessMWanHeader();
+
+    const char *class_name() const { return "ProcessMWanHeader"; }
+    const char *port_count() const { return PORTS_1_1; }
+    const char *processing() const { return AGNOSTIC; }
+  
+    int configure_phase() const { return ProgScheduler::CONFIGURE_PHASE + 1; }
+    int configure(Vector<String> &, ErrorHandler *);
+    int initialize(ErrorHandler *);
+    void add_handlers();
+    void cleanup(CleanupStage);
+
+    Packet *simple_action(Packet*);
+
+private:
+    ProgScheduler *_elem_ps;
+
+    unsigned int _max_paint;
+    uint32_t *_bandwidths;
+    int *_schedule;
+
+    bool update_schedule(int size, uint32_t *bandwidths);
+
+    static bool static_ary_equals(int size, int *a, int *b);
+};
+
+CLICK_ENDDECLS
+#endif
